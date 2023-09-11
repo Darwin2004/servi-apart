258fcd2d6b7c1d7edc41d93d006b0e43e46e313b
camilo pinilla <camilo.pinilla2503@gmail.com>
2023-09-11 11:37:43 -0500

    Arreglos carlos

--- Views/Administrador/menu-include.php
+++ Views/Administrador/menu-include.php
@@ -106,22 +106,34 @@
 
                     <li>
                         <a class="sidebar-sub-toggle">
-                            <i class="ti-info-alt"></i> Publicaciones
+                            <i class="ti-comment-alt"></i> Publicaciones
                             <span class="sidebar-collapse-icon ti-angle-down"></span>
                         </a>
                         <ul>
                             <li>
-                                <a href="#"><i class="ti-eye"></i>Crear Publicacion</a>
+                                <a href="#"><i class="ti-pencil-alt"></i>Crear Publicacion</a>
                             </li>
                         </ul>
                         <ul>
                             <li>
-                                <a href=#p"><i class="ti-eye"></i>Ver Publicaciones</a>
+                                <a href=#p"><i class="ti-book"></i>Ver Publicaciones</a>
                             </li>
                         </ul>
                         <ul>
                             <li>
-                                <a href="#"><i class="ti-eye"></i>Eliminar Publicaciones</a>
+                                <a href="#"><i class="ti-close"></i>Eliminar Publicaciones</a>
+                            </li>
+                        </ul>
+                    </li>
+
+                    <li>
+                        <a class="sidebar-sub-toggle">
+                            <i class="ti-money"></i> Pagos
+                            <span class="sidebar-collapse-icon ti-angle-down"></span>
+                        </a>
+                        <ul>
+                            <li>
+                                <a href="pagos.php"><i class="ti-eye"></i>Gestión de pagos</a>
                             </li>
                         </ul>
                     </li>
