abce7760b56130f75a1d2a98a0fd2caf875ad027
Franklin <andres10cortes07@gmail.com>
2023-09-16 16:46:09 -0500

    arreglo en carrusel de fotos vehiculo

--- Views/Administrador/fotos-vehiculo.php
+++ Views/Administrador/fotos-vehiculo.php
@@ -39,7 +39,9 @@ require_once("../../Controllers/mostrarInfoAdmin.php");
 
   <link href="../dashboard/css//lib/helper.css" rel="stylesheet">
   <link href="../dashboard/css/style.css" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <link rel="stylesheet" href="../../assets/css/style.css">
+  
 
 
 </head>
@@ -73,6 +75,10 @@ require_once("../../Controllers/mostrarInfoAdmin.php");
         <button type="submit" class="btn btn-primary">Search</button>
       </form>
     </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <!-- jquery vendor -->
     <script src="js/lib/jquery.min.js"></script>
     <script src="js/lib/jquery.nanoscroller.min.js"></script>
