a98738df3785ce4527a67f43ef5a1da8a609fce8
Johan Castañeda <cristianmarinmarin20005@gmail.com>
2023-09-16 17:18:06 -0500

    salon comunal en usuario funciona todo.

--- Views/Usuarios/salon-comunal.php
+++ Views/Usuarios/salon-comunal.php
@@ -40,7 +40,7 @@ require_once("../../Models/consultas.php");
     <link href="../dashboard/css/lib/helper.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-    <link href="../dashboard/css/style.css" rel="stylesheet">
+    <link href="../../dashboard/css/style.css" rel="stylesheet">
 </head>
 
 <body>
